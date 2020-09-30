15312f6ecd75063acb2700e588d5a1a5ed203dd6
Yurii Verbitskii <km1111111@yandex.ru>
2020-10-01 01:21:16 +0300

    fix: fix code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@
 module.exports = function towelSort (matrix) {
 
   if (matrix && matrix.length) {
-    let arr = [];
+
     for (let i = 0; i < matrix.length; i++) {
         if (i % 2 !== 0) {
-            arr.push(matrix[i].reverse());
+          matrix[i].reverse();
         }
     }
     return matrix = [].concat(...matrix);
